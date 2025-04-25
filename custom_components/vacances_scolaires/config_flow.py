dc40431bdaa7f91b861bcb18349e9c484a9fd70a
Master13011 <64130004+Master13011@users.noreply.github.com>
2025-04-25 15:37:53 +0200

    Update config_flow.py

--- custom_components/vacances_scolaires/config_flow.py
+++ custom_components/vacances_scolaires/config_flow.py
@@ -6,6 +6,8 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.core import HomeAssistant
+import logging
 
 from .const import (
     DOMAIN,
@@ -19,6 +21,8 @@ from .const import (
     CONF_VERIFY_SSL,
     ZONE_OPTIONS
 )
+
+_LOGGER = logging.getLogger(__name__)
 
 class VacancesScolairesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Vacances Scolaires."""
@@ -93,7 +97,8 @@ class VacancesScolairesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         
     # Ajout de l'OptionsFlow pour modifier update_interval et verify_ssl après configuration
     @staticmethod
-    def async_get_options_flow(config_entry):
+    def async_get_options_flow(config_entry: config_entries.ConfigEntry) -> config_entries.OptionsFlow:
+        """Create the options flow."""
         return VacancesScolairesOptionsFlowHandler(config_entry)
         
 class VacancesScolairesOptionsFlowHandler(config_entries.OptionsFlow):
@@ -106,12 +111,20 @@ class VacancesScolairesOptionsFlowHandler(config_entries.OptionsFlow):
     async def async_step_init(self, user_input: dict[str, Any] | None = None) -> FlowResult:
         """Manage the options."""
         if user_input is not None:
+            old_options = self.config_entry.options
+            
+            if user_input.get(CONF_VERIFY_SSL) != old_options.get(CONF_VERIFY_SSL, True):
+                _LOGGER.info(f"Option SSL changed from {old_options.get(CONF_VERIFY_SSL, True)} to {user_input.get(CONF_VERIFY_SSL)}")
+
+            if user_input.get(CONF_UPDATE_INTERVAL) != old_options.get(CONF_UPDATE_INTERVAL, self.config_entry.data.get(CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL)):
+                _LOGGER.info(f"Update interval changed from {old_options.get(CONF_UPDATE_INTERVAL, self.config_entry.data.get(CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL))} to {user_input.get(CONF_UPDATE_INTERVAL)}")
+
             return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
                 vol.Required(CONF_UPDATE_INTERVAL, default=self.config_entry.options.get(CONF_UPDATE_INTERVAL, self.config_entry.data.get(CONF_UPDATE_INTERVAL, DEFAULT_UPDATE_INTERVAL))): int,
-                vol.Optional(CONF_VERIFY_SSL, default=self.config_entry.options.get(CONF_VERIFY_SSL, True)): bool,  # Option SSL ici aussi
+                vol.Optional(CONF_VERIFY_SSL, default=self.config_entry.options.get(CONF_VERIFY_SSL, True)): bool,
             })
         )
